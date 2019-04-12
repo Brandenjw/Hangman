b548b5d0ab75232bbc1507641238fa1699a68059
Branden White <Brandenjwhite@gmail.com>
2019-04-12 14:32:51 -0400

    remade array for game words

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ function pageOne(){
     location.replace("index.html")
 }
 
-function ghostWords(){
-    onclick="document.getElementsbyId"('array').style.display='block';
-}
+// function ghostWords(){
+//     onclick="document.getElementsbyId"('array').style.display='block';
+// }
+let usedLetters = []
+//this is array of words to be used in game
+let keyWords = ["bangarang", "banshee","bogeyman","ghost","gargoyle","mummy","titan","undead"]
+
+
+
