a3f838ef2bcf2e45b27f292ffe093508df0328e8
Branden White <Brandenjwhite@gmail.com>
2019-04-12 15:04:43 -0400

    added js functions for accounting for word length and letters left

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ function pageOne(){
 // function ghostWords(){
 //     onclick="document.getElementsbyId"('array').style.display='block';
 // }
-let usedLetters = []
-//this is array of words to be used in game
-let keyWords = ["bangarang", "banshee","bogeyman","ghost","gargoyle","mummy","titan","undead"]
+// let usedLetters = [];
+// //this is array of words to be used in game
+// let keyWords = [ "banshee","bogeyman","ghost","gargoyle","mummy","titan","undead"]
+// //this allows for words to be chosen at random
+// let randomWord = keyWords[Math.floor(Math.random() * keyWords.length)];
+// //this accounts for number of letters per word on screen
+// let answers = [];
+// for(let i = 0; 0 < randomWord.length; i++){
 
+//     answers[i]= "_";
+// }
+// //displays guesses left
+// let lettersLeft = randomWord.length;
+
+// while(lettersLeft > 0)
 
 
