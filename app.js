f0730a207a33b50ed7c50c730c04b6467e656862
Branden White <Brandenjwhite@gmail.com>
2019-04-15 00:10:58 -0400

    added else statement

--- app.js
+++ app.js
@@ -13,21 +13,37 @@ const alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 for(var i = alphabetArray.length - 1; i >= 0; i--){
     $(".alphabetList").prepend("<div class='lettersType'>" + alphabetArray[i] + "</div>")
 }
+let word = ["b","o","o","g","e","y","m","a","n"]
+for(let i = 0 ; i < word.length; i++){
+    $(".keyEntry").append("<p class='userAnswer'> - </p>")
+}
+
 let checkLetter =()=>{
     let value = $(event.target).html()
-    // console.log(value)
-let word = ["b","o","o","g","e","y","m","a","n"]
-    for(let i = 0 ; i < word.length; i++){
-        if(value === word[i]){
-        console.log(value)
+    $(".keyEntry").empty()
+    //this allows for only letters in keyword to be typed
 
-            //   return true;
+    for(let i = 0 ; i < word.length; i++){
+         if(value != word[i] ){
+            $(".keyEntry").append("<p class='userAnswer'> - </p>")
         }
+
+        else if(value === word[i]){
+           console.log(value)
+        //    $(".keyEntry").remove()
+            $(".keyEntry").append("<p class='correctLetter'>" + value + "</p>")
+            
+        }
+         else{
+             console.log(value)
+             $(".usedLets").append("<p class='wrongOnes'>" + value + "</p>")
+            }
+         }
     }
-}
+
 $(".alphabetList").on("click", ".lettersType", checkLetter)
 
-
+// replaceChildText(document.getElementsByClassName('correctWord'),'findText','replacement');
 
 // const guessedWord = function (keyWords, userImput) {
 //     // if(userImput)
@@ -41,10 +57,11 @@ $(".alphabetList").on("click", ".lettersType", checkLetter)
 
 // }
 // var rightAnswer = [];
-// for (var i = 0; i < randomWord.length; i++) {
+// for (var i = 0; i < word.length; i++) {
 
 //     rightAnswer[i] = "_";
 // }
+// console.log(rightAnswer)
 
 
 
