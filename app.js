4002514ce6e691b97c09f42f2d139ddb75a7dab2
Branden White <Brandenjwhite@gmail.com>
2019-06-23 17:59:25 -0400

    rearranged the array for answers

--- app.js
+++ app.js
@@ -7,80 +7,10 @@ function pageOne() {
     location.replace("index.html")
 }
 //array for alphabet
-const alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-//creates a div for each letter typed and displays it on screen
-for(var i = alphabetArray.length - 1; i >= 0; i--){
-    $(".alphabetList").prepend("<div class='lettersType'>" + alphabetArray[i] + "</div>")
-}
-var word = ["b","o","o","g","e","y","m","a","n"]
-var correctLetters = [] 
-for(let i = 0 ; i < word.length; i++){
-    $(correctLetters).push('"-"')
-    
-}
-$(".keyEntry").append(correctLetters)
-var checkLetter =()=>{
-    var value = $(event.target).html()
-    
-    //this allows for only letters in keyword to be typed
-
-    for(let i = 0 ; i < word.length; i++){
-        
-
-        if(value === word[i]){
-           console.log(value)
-           correctLetters[i].push(value)
-        }
-       else{
-            console.log("wrong letter = " + value)
-        }
-    }
-    console.log(correctLetters)
-    $(".usedLets").append("<p class='wrongOnes'>" + value + "</p>")
-    }
-
-$(".alphabetList").on("click", ".lettersType", checkLetter)
-
-// replaceChildText(document.getElementsByClassName('correctWord'),'findText','replacement');
-
-// const guessedWord = function (keyWords, userImput) {
-//     // if(userImput)
-
-//     var keyWords = ['b', 'o', 'g', 'e', 'y', 'm', 'a', 'n'];
-
-//     for (let i = 0; i > keyWords.length; i++) {
-//         var key = keyWords[i].toLowerCase();
-// i
-//     }
-
-// }
-// var rightAnswer = [];
-// for (var i = 0; i < word.length; i++) {
-
-//     rightAnswer[i] = "_";
-// }
-// console.log(rightAnswer)
+const wordArrays =[ ["B","O","O","G","E","Y","M","A","N"],
+["G","H","O","S","T","S"],["Z","O","M","B","I","E","S"]]
 
 
 
 
 
-
-// function ghostWords(){
-//     onclick="document.getElementsbyId"('array').style.display='block';
-// }
-//this is array for letters incorrect and not in keyword
-var usedLetters = [];
-//this is array of words to be used in game
-// var keyWords = ["banshee", "bogeyman", "ghost", "gargoyle", "mummy", "titan", "undead"]
-//this allows for words to be chosen at random
-// var randomWord = keyWords[Math.floor(Math.random() * keyWords.length)];
-//this accounts for number of letters per word on screen
-//displays guesses left
-// var lettersLeft = randomWord.length;
-
-// while(lettersLeft > 0){
-// let heyYou = prompt("guess a type of monster");
-// }
-// if()
